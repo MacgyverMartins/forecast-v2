9d26c66e6230b2a9477ca0ad76820629c9baab23
Macgyver <macgyver.martins@gmail.com>
2022-03-02 19:45:19 -0400

    add theme config

---
+++ src/assets/theme.js
@@ -0,0 +1,12 @@
+const colors = {
+  black: `#000`,
+  white: `#FFF`,
+  purple: `#BF5AF2`,
+  yellow: `#FFD60A`,
+  blue: `#0A84FF`,
+  cyan: `#64D2FF`,
+};
+
+export const theme = {
+  colors,
+};
